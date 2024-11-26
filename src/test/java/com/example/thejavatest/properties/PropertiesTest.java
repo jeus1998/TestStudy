f0cce1171bc1244401f22e71222a1ff2c369e762
jeus1998 <zeus20240228@gmail.com>
2024-11-26 22:00:17 +0900

    [2024:11:26] Junit5 설정 파일

---
+++ src/test/java/com/example/thejavatest/properties/PropertiesTest.java
@@ -0,0 +1,18 @@
+package com.example.thejavatest.properties;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class PropertiesTest {
+    @Test
+    @Disabled
+    void test1(){
+    }
+    @Test
+    void test2(){
+    }
+    @Test
+    void test_study_is_fun(){
+
+    }
+}
