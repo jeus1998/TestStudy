5f8df025b8c24327ac731fac7d1e074939892e81
jeus1998 <zeus20240228@gmail.com>
2024-11-27 21:27:33 +0900

    [2024:11:27] Mockito stubbing 연습문제

--- src/test/java/com/example/thejavatest/study/StudyServiceTest.java
+++ src/test/java/com/example/thejavatest/study/StudyServiceTest.java
@@ -2,6 +2,7 @@ package com.example.thejavatest.study;
 
 import com.example.thejavatest.domain.Member;
 import com.example.thejavatest.domain.Study;
+import com.example.thejavatest.domain.StudyStatus;
 import com.example.thejavatest.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -32,5 +33,25 @@ class StudyServiceTest {
         assertThrows(RuntimeException.class, () -> memberService.findById(2L));
         assertEquals(Optional.empty(), memberService.findById(3L));
         assertEquals(Optional.empty(), memberService.findById(4L));
+    }
+    @Test
+    void test(@Mock MemberService memberService, @Mock StudyRepository studyRepository){
+        // TODO memberService 객체에 findById 메소드를 1L 값으로 호출하면 Optional.of(member) 객체를 리턴하도록 Stubbing
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("baejeu@naver.com");
+        when(memberService.findById(1L)).thenReturn(Optional.of(member));
+
+        // TODO studyRepository 객체에 save 메소드를 study 객체로 호출하면 study 객체 그대로 리턴하도록 Stubbing
+        Study study = new Study();
+        study.setId(1L);
+        study.setStatus(StudyStatus.STARTED);
+
+        when(studyRepository.save(study)).thenReturn(study);
+
+        StudyService studyService = new StudyService(memberService, studyRepository);
+        Study createStudy = studyService.createNewStudy(1L, study);
+
+        assertEquals(member.getId(), createStudy.getOwnerId());
     }
 }
