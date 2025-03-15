3fd73c0a61b64b2838209ae525fa071739d874ce
baejeu <zeus20240228@gmail.com>
2025-03-15 23:41:05 +0900

    [2025:03:15] Mockito mock 객체 Stubbing

--- study-test/src/test/java/spring/studytest/mockito/StudyServiceTest.java
+++ study-test/src/test/java/spring/studytest/mockito/StudyServiceTest.java
@@ -6,10 +6,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import spring.studytest.domain.Member;
+import spring.studytest.domain.Study;
 import spring.studytest.member.MemberService;
 import spring.studytest.study.StudyRepository;
 import spring.studytest.study.StudyService;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
@@ -19,6 +26,28 @@ class StudyServiceTest {
     @Mock MemberService memberService;
     @Mock StudyRepository studyRepository;
     @InjectMocks StudyService studyService;
+
+    @Test
+    @DisplayName("main test")
+    void main(){
+        var member = new Member();
+        member.setId(1L);
+        member.setEmail("test@test.com");
+
+        when(memberService.findById(any()))
+                .thenReturn(Optional.of(member))    // 1
+                .thenThrow(RuntimeException.class)  // 2
+                .thenReturn(Optional.empty());   // 3
+
+        assertAll(
+                () -> assertThat(memberService.findById(1L)).isPresent(),
+                () -> assertThatThrownBy(() -> memberService.findById(1L))
+                        .isInstanceOf(RuntimeException.class),
+                () -> assertThat(memberService.findById(1L)).isEmpty(),
+                () -> assertThat(memberService.findById(1L)).isEmpty()
+        );
+    }
+
 
     @Test
     @DisplayName("Mockito.mock")
