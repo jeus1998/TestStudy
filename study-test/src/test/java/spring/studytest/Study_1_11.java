faf4b9821771b87aba64d9ac3a9cc7f9149d636c
baejeu <zeus20240228@gmail.com>
2025-03-15 18:48:39 +0900

    [2025:03:15] JUnit5 테스트 순서 공부

---
+++ study-test/src/test/java/spring/studytest/Study_1_11.java
@@ -0,0 +1,28 @@
+package spring.studytest;
+
+import org.junit.jupiter.api.*;
+
+/**
+ * JUnit 5 테스트 순서
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class Study_1_11 {
+    @Order(1)
+    @Test
+    void test1() {
+        System.out.println(1);
+    }
+
+    @Order(2)
+    @Test
+    void test2() {
+        System.out.println(2);
+    }
+
+    @Order(3)
+    @Test
+    void test3() {
+        System.out.println(3);
+    }
+}
